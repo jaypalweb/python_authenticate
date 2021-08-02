586330664abce155ac127f5d448ee71ea0cbe2f1
Jay Pal <contactjaypal@gmail.com>
2021-08-03 00:40:48 +0530

    login logic

--- authenticate/views.py
+++ authenticate/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 
 def home(request):
     return render(request, 'authenticate/home.html', {})
 
 def login_user(request):
-    return render(request, 'authenticate/login.html', {})
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # messages.success(request, ('You Have Been Logged In!'))
+            return redirect('home')
+        else:
+            # messages.success(request, ('Error Logging In - Please Try Again...'))
+            return redirect('login')
+    else:
+        return render(request, 'authenticate/login.html', {})
